1bae30589faf104a27eacf9ee07806e1cb3d685c
Pranesh98 <praneshchy23@gmail.com>
2024-06-11 04:16:05 +0600

    navbar and footer design done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Home/Home/Shared/Footer/Footer";
+import NavBar from "../pages/Home/Home/Shared/Footer/NavBar/NavBar";
 
 const Main = () => {
     return (
         <div>
-            <h1>First page</h1>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { RouterProvider } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )
